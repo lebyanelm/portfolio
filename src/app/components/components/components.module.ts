c81e246b139ffd7901928d8de8ce9eaf1bafea94
Libby Lebyane <lebyanelm@nextify.co.za>
2021-07-07 21:59:04 +0200

    Added: Footer component

--- src/app/components/components/components.module.ts
+++ src/app/components/components/components.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import {AnimatedBackgroundComponent} from '../animated-background/animated-background.component';
+import { FooterComponent } from '../footer/footer.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    AnimatedBackgroundComponent
+    AnimatedBackgroundComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    AnimatedBackgroundComponent
+    AnimatedBackgroundComponent,
+    FooterComponent
   ]
 })
 export class ComponentsModule { }
